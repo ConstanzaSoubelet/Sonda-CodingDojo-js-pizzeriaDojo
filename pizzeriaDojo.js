c6742d3967afb5322193837bda692fcc037ae003
ConstanzaSoubelet <conniesoubelet@gmail.com>
2023-09-19 20:37:53 -0300

    Subir js - PizzeriaDojo

---
+++ pizzeriaDojo.js
@@ -0,0 +1,82 @@
+function pizzaOven(pan, salsas, cheeses, toppings) {
+    var pizza = {};
+    pizza.pan = pan;
+    pizza.salsas = salsas;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var pizza1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+var pan = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var salsas = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.pan = randomPick(pan);
+    pizza.salsas = randomPick(salsas);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
